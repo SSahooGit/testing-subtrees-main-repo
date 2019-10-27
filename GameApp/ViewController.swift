860472c43b7881680683c655f8307dad10f8fbe8
Sahoo, Susanta Kumar <Iejss@arity.com>
2019-10-27 12:30:26 +0530

    GameApp:1st commit

---
+++ GameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameApp
+//
+//  Created by Sahoo, Susanta Kumar on 27/10/19.
+//  Copyright © 2019 Sahoo, Susanta Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("This is game App. Master")
+    }
+}
+
