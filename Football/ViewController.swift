ad164e1c3c6286fda9921790e51df47dcfaae98d
Sahoo, Susanta Kumar <Iejss@arity.com>
2019-10-30 10:34:20 +0530

    Squashed 'feature2/' content from commit 2ca5dc0
    git-subtree-dir: feature2
    git-subtree-split: 2ca5dc094a575c3ff3a1183af1c0f479fe97c811

---
+++ Football/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Football
+//
+//  Created by Sahoo, Susanta Kumar on 29/10/19.
+//  Copyright © 2019 Sahoo, Susanta Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("This is football application")
+    }
+
+
+}
+
